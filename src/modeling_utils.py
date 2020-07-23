0516c7cdac0346461c14b3ffbc20020563451241
heehehe <heeseon0315@gmail.com>
2020-07-23 22:04:05 +0900

    Fix modeling_utils.py

--- src/modeling_utils.py
+++ src/modeling_utils.py
@@ -20,3 +20,9 @@ def add_var(row, var, var2, var_dict, popular_var, date):
         var_list.extend(var_list_update)
         var_list = var_list[:100]
     return var_list
+
+def add_by_cf_dic(row, cf_dic):
+    row_num = row['id']
+    cur_song = remove_seen(row['songs'], cf_dic[row_num])[:100]
+    return cur_song    
+    
